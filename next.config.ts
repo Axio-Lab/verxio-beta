888c737bb3897a20b5e32d186b426e2639b2ad4f
amdonatusprince <donatusprince@gmail.com>
2025-09-26 22:08:53 +0800

    added support for batch creation of reward links

--- next.config.ts
+++ next.config.ts
@@ -4,16 +4,11 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true, // Disable ESLint during builds
   },
-};
-
-export default nextConfig;
-
-/** @type {import('next').NextConfig} */
- 
-module.exports = {
   experimental: {
     serverActions: {
       bodySizeLimit: '2mb',
     },
   },
-}
+};
+
+export default nextConfig;
